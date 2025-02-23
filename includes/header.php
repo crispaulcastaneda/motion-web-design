fe7fae5133e9594341cc70272045759079b730d1
crispaulcastaneda <cristianpaul_c@yahoo.com>
2025-02-23 22:07:28 +0800

    V1.0 | Added transition to the topic claiming the explanation and other tips.

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,19 @@
     <nav>
       <ul>
         <li>Home</li>
+        <li class="list--item-hover">Services <i class='bx bx-chevron-down'></i>
+          <ul>
+            <li>Branding</li>
+            <li>Web Design</li>
+            <li>Web Development</li>
+          </ul>
+        </li>
         <li>About</li>
         <li>Contact</li>
       </ul>
     </nav>
+    <button class="header--btn">
+      Get A Quote
+    </button>
   </div>
 </header>
