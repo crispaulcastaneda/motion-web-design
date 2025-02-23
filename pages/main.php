8826a7d201b5b51ad8714771e062450b50c1257c
crispaulcastaneda <cristianpaul_c@yahoo.com>
2025-02-23 20:21:33 +0800

    V1.0 | WM Video Product | Optimization and more

---
+++ pages/main.php
@@ -0,0 +1,12 @@
+<section class="section--block-hero">
+  <div class="inner">
+
+    <video autoplay muted loop>
+      <source src="./assets/images/coca-cola.mp4" type="video/mp4">
+      <source src="./assets/images/coca-cola.webm" type="video/webm">
+    </video>
+
+    <h1>Lorem ipsum dolor</h1>
+    <h2>consectetur adipiscing elit sed do </h2>
+  </div>
+</section>
